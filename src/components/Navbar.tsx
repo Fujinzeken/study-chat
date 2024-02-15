d9ecee50815538b9508cecdfb1d51d07a1b2e975
micheal <fujinzeken@yahoo.com>
2024-02-15 13:26:03 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { MaxWidthWrapper } from "./MaxWidthWrapper";
+import Link from "next/link";
+import { Button, buttonVariants } from "./ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            Study<span className="text-blue-600">₡hat.</span>
+          </Link>
+
+          {/* Todo: add to mobile navbar  */}
+
+          <div className="hidden items-center space-x-4 sm:flex">
+            <Link href="/pricing">
+              <Button size="sm" variant="ghost">
+                Pricing
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
